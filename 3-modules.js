31ac6f8ee08fa00ba31379e9eef9662cad3e83d9
Enrique García <enriquegarcia2733@gmail.com>
2022-08-24 15:42:04 -0500

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require('./app2');
+const saluda = require('./utils');
+const datosAlt = require('./alernative');
+require('./granada')
+
+saluda("susan");
+saluda(names.kike);
+saluda(names.alma);
---
+++ app2.js
@@ -0,0 +1,5 @@
+const secret = "Super secret";
+const kike = "kike";
+const alma = "alma";
+
+module.exports = {kike,alma}
